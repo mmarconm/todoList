84b6f3e92d2b57bc5b057590092ba5cf8ab0c0aa
Marcelo Marcon <marconm.inf0@gmail.com>
2019-04-21 00:02:37 -0400

    Mark the list as done

--- index.php
+++ index.php
@@ -47,9 +47,9 @@ $items = $itemsQuery->rowCount() ? $itemsQuery : [];
         <?php foreach($items as $item): ?>
             <li>
                 <span class="item <?php echo $item['done'] ? ' done' : '' ?>"><?php echo $item['name']; ?></span>
-                
+
                 <?php if(!$item['done']): ?>
-                    <a href="#" class="done-button">Mark as Done!</a>
+                    <a href="mark.php?as=done&item=<?php echo $item['id']; ?>" class="done-button">Mark as Done!</a>
                 <?php endif; ?>
             </li>
         <?php endforeach; ?>
