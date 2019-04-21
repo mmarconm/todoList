e6f826f5a6e2226b991d992143ed9f243b4e531a
Marcelo Marcon <marconm.inf0@gmail.com>
2019-04-21 12:07:08 -0400

    css for delete button

--- index.php
+++ index.php
@@ -47,18 +47,18 @@ $items = $itemsQuery->rowCount() ? $itemsQuery : [];
         <?php foreach($items as $item): ?>
             <li>
 
-            <!-- Obter o id do elemento e deletar do banco e retornar para index.php -->
-            <a class="delete-button" href="delete.php?item_id=<?php echo $item['id']; ?>">Delete</a>
-
-            <span class="item <?php echo $item['done'] ? ' done' : '' ?>"><?php echo $item['name']; ?></span>
-
+                <span class="item <?php echo $item['done'] ? ' done' : '' ?>"><?php echo $item['name']; ?></span>
                 <?php if(!$item['done']): ?>
                     <a href="mark.php?as=done&item=<?php echo $item['id']; ?>" class="done-button">Mark as Done!</a>
-
                 <?php elseif($item['done']): ?>
                     <a href="mark.php?as=notdone&item=<?php echo $item['id']; ?>" class="done-button">Undone!</a>
-                
                 <?php endif; ?>
+
+            
+                <div class="buttons">
+                    <!-- Obter o id do elemento e deletar do banco e retornar para index.php -->
+                    <a class="delete-button" href="delete.php?item_id=<?php echo $item['id']; ?>">Delete</a>
+                </div>
             </li>
         <?php endforeach; ?>
     </ul>
